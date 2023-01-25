0d29353285e7954d57f2d5e591610931c61811fc
Mariano Cicchinelli <blazeofanger@gmail.com>
2023-01-25 16:45:01 +0100

    Removed unnecessary break's and updated libraries.

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module chip-8
 
 go 1.19
 
-require github.com/hajimehoshi/ebiten/v2 v2.4.15
+require github.com/hajimehoshi/ebiten/v2 v2.4.16
 
 require (
 	github.com/ebitengine/purego v0.1.1 // indirect
@@ -11,7 +11,7 @@ require (
 	github.com/hajimehoshi/oto/v2 v2.3.1 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/exp/shiny v0.0.0-20230108222341-4b8118a2686a // indirect
+	golang.org/x/exp/shiny v0.0.0-20230124195608-d38c7dcee874 // indirect
 	golang.org/x/image v0.2.0 // indirect
 	golang.org/x/mobile v0.0.0-20221110043201-43a038452099 // indirect
 	golang.org/x/sys v0.4.0 // indirect
--- pkg/chip8/emulator.go
+++ pkg/chip8/emulator.go
@@ -106,85 +106,63 @@ func (emulator *Emulator) execute(opcode uint16) {
 		return
 	case digit1 == 0 && digit2 == 0 && digit3 == 0xE && digit4 == 0: // 00E0 - Clear screen
 		emulator.display.Clear()
-		break
 	case digit1 == 0 && digit2 == 0 && digit3 == 0xE && digit4 == 0xE: // 00EE - Return from Subroutine
 		emulator.pc = emulator.stack.Pop()
-		break
 	case digit1 == 1: // 1NNN - Jump
 		emulator.pc = opcode & 0xFFF
-		break
 	case digit1 == 2: // 2NNN - Call Subroutine
 		emulator.stack.Push(emulator.pc)
 		emulator.pc = opcode & 0xFFF
-		break
 	case digit1 == 3: // 3XNN - Skip next if VX == NN
 		nn := uint8(opcode & 0xFF)
 		if emulator.registers[digit2] == nn {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 4: // 4XNN - Skip next if VX != NN
 		nn := uint8(opcode & 0xFF)
 		if emulator.registers[digit2] != nn {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 5: // 5XY0 - Skip next if VX == VY
 		if emulator.registers[digit2] == emulator.registers[digit3] {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 6: // 6XNN - VX = NN
 		emulator.registers[digit2] = uint8(opcode & 0xFF)
-		break
 	case digit1 == 7: // 7XNN - VX += NN (carry flag is not changed)
 		emulator.registers[digit2] += uint8(opcode & 0xFF)
-		break
 	case digit1 == 8: // 8XY0 - VX = VY
 		emulator.registers[digit2] = emulator.registers[digit3]
-		break
 	case digit1 == 8 && digit4 == 1: // 8XY1 -	VX |= VY
 		emulator.registers[digit2] |= emulator.registers[digit3]
-		break
 	case digit1 == 8 && digit4 == 2: // 8XY2 - VX &= VY
 		emulator.registers[digit2] &= emulator.registers[digit3]
-		break
 	case digit1 == 8 && digit4 == 3: // 8XY3 - VX ^= VY
 		emulator.registers[digit2] ^= emulator.registers[digit3]
-		break
 	case digit1 == 8 && digit4 == 4: // 8XY4 - VX += VY (VF is set to 1 when there's a carry, and to 0 when there is not)
 		emulator.registers[digit2], emulator.registers[0xF] = utils.Add8(emulator.registers[digit2], emulator.registers[digit3], 0)
-		break
 	case digit1 == 8 && digit4 == 5: // 8XY5 - VX -= VY (VF is set to 0 when there's a borrow, and 1 when there is not)
 		emulator.registers[digit2], emulator.registers[0xF] = utils.Sub8(emulator.registers[digit2], emulator.registers[digit3], 0)
 		emulator.registers[0xF] ^= 0x1
-		break
 	case digit1 == 8 && digit4 == 6: // 8XY6 - VX »= 1 (Stores the least significant bit of VX in VF and then shifts VX to the right by 1)
 		emulator.registers[0xF] = emulator.registers[digit2] & 0x1
 		emulator.registers[digit2] >>= 1
-		break
 	case digit1 == 8 && digit4 == 7: // 8XY7 - VX = VY - VX (VF is set to 0 when there's a borrow, and 1 when there is not)
 		emulator.registers[digit2], emulator.registers[0xF] = utils.Sub8(emulator.registers[digit3], emulator.registers[digit2], 0)
 		emulator.registers[0xF] ^= 0x1
-		break
 	case digit1 == 8 && digit4 == 0xE: // 8XYE - VX «= 1 (Stores the most significant bit of VX in VF and then shifts VX to the left by 1)
 		emulator.registers[0xF] = (emulator.registers[digit2] >> 7) & 0x1
 		emulator.registers[digit2] <<= 1
-		break
 	case digit1 == 9 && digit4 == 0: // 9XY0 - Skip if VX != VY
 		if emulator.registers[digit2] != emulator.registers[digit3] {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 0xA: // ANNN - I = NNN
 		emulator.i = opcode & 0xFFF
-		break
 	case digit1 == 0xB: // BNNN - PC = V0 + NNN (Jump to V0 + NNN)
 		emulator.pc = uint16(emulator.registers[0x0]) + (opcode & 0xFFF)
-		break
 	case digit1 == 0xC: // CXNN - VX = rand() & NN
 		emulator.registers[digit2] = uint8(rand.Intn(math.MaxUint8+1)) & uint8(opcode&0xFF)
-		break
 	case digit1 == 0xD: // DXYN - Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels
 		// Get coords for our sprite
 		xCord := uint16(emulator.registers[digit2])
@@ -205,20 +183,16 @@ func (emulator *Emulator) execute(opcode uint16) {
 				}
 			}
 		}
-		break
 	case digit1 == 0xE && digit3 == 9 && digit4 == 0xE: // EX9E - Skips the next instruction if the key stored in VX is pressed
 		if emulator.input.IsKeyPressed(emulator.registers[digit2]) {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 0xE && digit3 == 0xA && digit4 == 1: // EXA1 - Skips the next instruction if the key stored in VX is not pressed
 		if !emulator.input.IsKeyPressed(emulator.registers[digit2]) {
 			emulator.pc += 2
 		}
-		break
 	case digit1 == 0xF && digit3 == 0 && digit4 == 7: // FX07 - VX = DT (Sets VX to the value of the delay timer)
 		emulator.registers[digit2] = emulator.delayTimer
-		break
 	case digit1 == 0xF && digit3 == 0 && digit4 == 0xA: // FX0A - Wait for Key Press (blocking operation, all instruction halted until next key event)
 		key, pressed := emulator.input.IsAnyKeyPressed()
 		if pressed {
@@ -227,19 +201,14 @@ func (emulator *Emulator) execute(opcode uint16) {
 			// Redo opcode
 			emulator.pc -= 2
 		}
-		break
 	case digit1 == 0xF && digit3 == 1 && digit4 == 5: // FX15 - DT = VX (Sets the delay timer to VX)
 		emulator.delayTimer = emulator.registers[digit2]
-		break
 	case digit1 == 0xF && digit3 == 1 && digit4 == 8: // FX18 - ST = VX (Sets the sound timer to VX)
 		emulator.soundTimer = emulator.registers[digit2]
-		break
 	case digit1 == 0xF && digit3 == 1 && digit4 == 0xE: // FX1E - I += VX (VF is not affected)
 		emulator.i += uint16(emulator.registers[digit2])
-		break
 	case digit1 == 0xF && digit3 == 2 && digit4 == 9: // FX29 - I = sprite_addr[VX] (Sets I to the location of the sprite for the character in VX)
 		emulator.i = uint16(emulator.registers[digit2]) * 5
-		break
 	case digit1 == 0xF && digit3 == 3 && digit4 == 3: // FX33 - I = BCD of VX (Stores the binary-coded decimal representation of VX, with the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2)
 		// Fetch the hundreds digit by dividing by 100 and tossing the decimal
 		emulator.ram.Write(emulator.i, emulator.registers[digit2]/100)
@@ -247,17 +216,14 @@ func (emulator *Emulator) execute(opcode uint16) {
 		emulator.ram.Write(emulator.i+1, emulator.registers[digit2]/10%10)
 		// Fetch the ones digit by tossing the hundreds and the tens
 		emulator.ram.Write(emulator.i+2, emulator.registers[digit2]%10)
-		break
 	case digit1 == 0xF && digit3 == 5 && digit4 == 5: // FX55 - Store V0 - VX into I (Stores from V0 to VX [included], in memory, starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified)
 		for idx := uint16(0); idx < digit2+1; idx++ {
 			emulator.ram.Write(emulator.i+idx, emulator.registers[idx])
 		}
-		break
 	case digit1 == 0xF && digit3 == 6 && digit4 == 5: // FX65 - Load I into V0 - VX (Fills from V0 to VX [included] with values from memory, starting at address I. The offset from I is increased by 1 for each value read, but I itself is left unmodified)
 		for idx := uint16(0); idx < digit2+1; idx++ {
 			emulator.registers[idx] = emulator.ram.Read(emulator.i + idx)
 		}
-		break
 	default:
 		log.Fatalf("Unimplemented opcode: %v", opcode)
 	}
